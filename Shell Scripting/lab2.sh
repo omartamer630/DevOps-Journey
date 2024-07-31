d6b53e4bdef1f9b35e1901122a73b9bae2f8d18e
omartamer630 <omartamerdev@gmail.com>
2024-07-31 04:17:53 +0300

    Removing Python Function

--- Shell Scripting/lab2.sh
+++ Shell Scripting/lab2.sh
@@ -2,21 +2,7 @@
 #! /usr/bin/python3
 
 #clear
-function prayerTimep() {
-res=`curl -L -s "http://api.aladhan.com/v1/timingsByCity?city=Alexandria&country=Arab%20Republic%20of%20Egypt" | json_pp -f json`
-  export json_file="$res"
-  python3 -c "
-import os
-import json
-
-x = os.environ['json_file']
-# Print the JSON data
-body = json.loads(x)['data']['timings']
-for i, j in body.items():
-    print(f'{i}: {j}')
-"
-}
-function prayerTimeb(){
+function prayerTime(){
 
 res=`curl -L -s "http://api.aladhan.com/v1/timingsByCity?city=Alexandria&country=Arab%20Republic%20of%20Egypt" | json_pp -f json`
 
@@ -24,4 +10,3 @@ echo "${res}" | jq '.data.timings'
 }
 
 prayerTimeb
-#prayerTimep

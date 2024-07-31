10cc0afddf1454aaa6dcf3b7514466351a30bbef
omartamer630 <omartamerdev@gmail.com>
2024-07-31 20:34:19 +0300

    Python Journey Added

---
+++ Automation/Python/PythonSyntax/ControlFlow.py
@@ -0,0 +1,37 @@
+#------------------------------
+# -- Control Flow --
+# -- If,Elif,Else --
+# -- Make Decisions --
+# -----------------------------
+
+# uName = input()
+# uCountry = input().capitalize()
+# uCity = input().capitalize()
+# cName = "Python Course"
+# cPrice = 100
+# cDicount = 0
+
+# if uCountry == ("Egypt" or "Afrikaans") :
+
+#   print(f"Hello {uName} from {uCountry}")
+#   print("================================")
+#   nam = cName[:cName.index(" ")]
+#   print(f"The Course \"{nam}\" Price: ${cPrice}")
+#   if uCity  == ( "Alexandria" or "Cairo" or "Bahira") :
+
+#     print(f"Hello {uName} from {uCity}/{uCountry}")
+#     print("================================")
+#     nam = cName[:cName.index(" ")]
+#     print(f"The Course \"{nam}\" Price: ${cPrice}")
+
+# else : 
+
+#   print("Country not supported")
+
+# Condition If True | If Condition | Else | Condition If False 
+# movieRate = 18
+# age = 16
+
+# print("Movie's Not Good 4u" if age < movieRate else "Movie's Good 4u")
+
+
